80cf58642afb9e09e6715febbbeddfdd359e7224
bhagyaravi <bhagyaravi98@gmail.com>
2020-07-21 11:24:47 +0530

    Update TripMessageProcessor.java

--- src/main/java/com/test/quarkus/rest/TripMessageProcessor.java
+++ src/main/java/com/test/quarkus/rest/TripMessageProcessor.java
@@ -3,6 +3,8 @@ package com.test.quarkus.rest;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import io.smallrye.reactive.messaging.annotations.Merge;
 import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import io.smallrye.reactive.messaging.annotations.Merge;
 
 @ApplicationScoped
 public class TripMessageProcessor {
