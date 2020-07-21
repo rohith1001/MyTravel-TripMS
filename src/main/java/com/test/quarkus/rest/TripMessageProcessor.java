b494f101e9901036385a7b05f77462d984b2e06c
bhagyaravi <bhagyaravi98@gmail.com>
2020-07-21 15:11:10 +0530

    Update TripMessageProcessor.java

--- src/main/java/com/test/quarkus/rest/TripMessageProcessor.java
+++ src/main/java/com/test/quarkus/rest/TripMessageProcessor.java
@@ -10,6 +10,8 @@ import org.json.simple.parser.*;
 import com.test.quarkus.model.Trip;
 import java.util.UUID;
 import io.smallrye.reactive.messaging.annotations.Blocking;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
 
 @ApplicationScoped
 public class TripMessageProcessor {
@@ -25,6 +27,9 @@ public class TripMessageProcessor {
 	    	System.out.println("Trip Id is " + jo.get("tripId"));
 	    	UUID tripId = UUID.fromString(((String)jo.get("tripId")));
 	        Trip trip = Trip.findById(tripId);
+		Jsonb jsonb = JsonbBuilder.create();
+		String result = jsonb.toJson(trip);
+		System.out.println("Trip Details are :" + trip);
 	    	String paymentstatus = (String)jo.get("status");
 	    	trip.update("tripStatus",  paymentstatus);
 		}catch(org.json.simple.parser.ParseException e){
