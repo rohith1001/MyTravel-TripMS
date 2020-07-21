9ee51eeda494d517979c5e4ea3fa45a2522cf80a
bhagyaravi <bhagyaravi98@gmail.com>
2020-07-21 11:19:49 +0530

    Create TripMessageProcessor.java

---
+++ src/main/java/com/test/quarkus/rest/TripMessageProcessor.java
@@ -0,0 +1,17 @@
+package com.test.quarkus.rest;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TripMessageProcessor {
+
+	
+    @Incoming("paymentstatus")
+    @Merge
+    public  void process(String paymentString) {
+		System.out.println("Payment status string is :" + paymentString);
+	
+	}
+}
